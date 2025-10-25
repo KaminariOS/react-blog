4c48ee2a52a1ae3fe99ff90eaf11f3baa1fb0c38
kosumi <chankocyo@gmail.com>
2025-10-24 21:53:24 -0400

    Update projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,29 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'rustracer',
+    description: `A PBR glTF 2.0 renderer based on Vulkan ray-tracing, written in Rust.`,
+    imgSrc: 'https://github.com/KaminariOS/rustracer/blob/main/images/lucy.png?raw=true',
+    href: 'https://github.com/KaminariOS/rustracer',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '@GoldStarWorldBot',
+    description: `A Telegram bot that monitors abrupt price movement of tickers including gold and SPX and Trump Truth Social posts in
+real time`,
+    imgSrc: 'https://github.com/KaminariOS/telebot/blob/master/assets/cover.png?raw=true',
+    href: 'https://github.com/KaminariOS/telebot',
+  },
+  {
+    title: 'Verustd',
+    description: "Formal verification of the functional correctness of Rust std BinaryHeap implementation",
+    imgSrc: 'https://verus-lang.github.io/verus/verus/assets/verus-color.svg',
+    href: 'https://github.com/KaminariOS/Verustd',
+  },
+  {
+    title: 'Tinper',
+    description: "A Dating App Simulator with 'Real' Profiles.",
+    imgSrc: 'https://github.com/KaminariOS/Tinper/raw/master/public/animation.gif',
+    href: 'https://github.com/KaminariOS/Tinper',
   },
 ]
 
